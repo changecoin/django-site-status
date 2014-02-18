072827c56e534c2a6ba2ea3ede109d8fd8c859a7
Mantas Vidutis <mantas.a.vidutis@gmail.com>
2014-02-18 10:14:45 -0800

    directory structure

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-site-status',
+    version='0.1',
+    packages=['site_status'],
+    include_package_data=True,
+    license='BSD License',  # example license
+    description='A simple Django app to issue site wide alerts',
+    long_description=README,
+    url='https://github.com/changecoin/django-site-status',
+    author='Mantas Vidutis',
+    author_email='mantas@turbines.io',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
