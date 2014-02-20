51d4dc4e6007e79f498fb6d51ffea3cb21c53c9c
Mantas Vidutis <mantas.a.vidutis@gmail.com>
2014-02-19 17:13:11 -0800

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-site-status',
-    version='0.1.1',
+    version='0.1.2',
     packages=['site_status'],
     include_package_data=True,
     license='BSD License',  # example license
